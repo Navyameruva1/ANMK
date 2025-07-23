2a4bd6dc82f5555a2d4580a13b7ffa5dc51a6b72
Navyameruva1 <navyameruva1@gmail.com>
2025-07-23 10:58:03 +0530

    Add files via strong

---
+++ strong.java
@@ -0,0 +1,39 @@
+public class strong{
+
+    // Function to calculate factorial
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            int fact = 1;
+            for (int i = 2; i <= n; i++) {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+
+    // Function to check if a number is strong
+    public static boolean isStrong(int num) {
+        int originalNum = num;
+        int sumOfFactorials = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sumOfFactorials += factorial(digit);
+            num /= 10;
+        }
+
+        return sumOfFactorials == originalNum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Strong numbers between 1 and 5000 are:");
+        for (int i = 1; i <= 5000; i++) {
+            if (isStrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); // For a new line at the end
+    }
+}
